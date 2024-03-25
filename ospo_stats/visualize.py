be7c8912b8910d7322123cda7fd6a6aca75acf9a
Jason Lo <lcmjlo@gmail.com>
2024-03-25 19:06:11 +0000

    add code for 2 new plots: stargazers and commits per year (data is still not complete yet)

--- ospo_stats/visualize.py
+++ ospo_stats/visualize.py
@@ -1,11 +1,25 @@
 import altair as alt
 import pandas as pd
-from ospo_stats.db import ENGINE
 
+from ospo_stats.db import ENGINE
 
 _query_repo_by_year = """
 SELECT strftime('%Y', created_at) AS year, COUNT(*) AS num_repos
 FROM repo 
+GROUP BY year
+ORDER BY year;
+"""
+
+_query_commit_by_year = """
+SELECT strftime('%Y', committed_at) AS year, COUNT(*) AS num_commits
+FROM commit_history
+GROUP BY year
+ORDER BY year;
+"""
+
+_query_stargazer_by_year = """
+SELECT strftime('%Y', starred_at) AS year, COUNT(*) AS num_stargazers
+FROM stargazer_history
 GROUP BY year
 ORDER BY year;
 """
@@ -20,7 +34,25 @@ def get_repo_by_year() -> pd.DataFrame:
     return df
 
 
-def plot_cumulative_repos(
+def get_commit_by_year() -> pd.DataFrame:
+    """Get the number of commits by year."""
+    with ENGINE.connect() as conn:
+        df = pd.read_sql(_query_commit_by_year, conn)
+
+    df["cumulative_n"] = df["num_commits"].cumsum()
+    return df
+
+
+def get_stargazer_by_year() -> pd.DataFrame:
+    """Get the number of stargazers by year."""
+    with ENGINE.connect() as conn:
+        df = pd.read_sql(_query_stargazer_by_year, conn)
+
+    df["cumulative_n"] = df["num_stargazers"].cumsum()
+    return df
+
+
+def plot_cumulative(
     df: pd.DataFrame,
     year_col_name: str = "year",
     cumulative_n_col_name: str = "cumulative_n",
@@ -30,15 +62,6 @@ def plot_cumulative_repos(
     return (
         alt.Chart(df)
         .mark_line(point=True)
-        .encode(
-            x=f"{year_col_name}:O",
-            y=alt.Y(
-                f"{cumulative_n_col_name}:Q", title="Cumulative Number of Repositories"
-            ),
-        )
-        .properties(
-            title="Yearly Growth of UW–Madison's Open-Source (public) Repositories on GitHub",
-            width=600,
-            height=400,
-        )
+        .encode(x=f"{year_col_name}:O", y=f"{cumulative_n_col_name}:Q")
+        .properties(width=600, height=400)
     )
